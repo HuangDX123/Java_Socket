4cc820a3875572ec87129019b6f2aba501bc6217
zero <1580359334@qq.com>
2020-10-03 04:02:33 +0800

    上传完整版

---
+++ src/聊天/UdpSenderDemo01.java
@@ -0,0 +1,35 @@
+package 聊天;
+//服务端
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+
+public class UdpSenderDemo01 {
+    public static void main(String[] args)throws Exception {
+        System.out.println("**********客户端**************");
+
+        DatagramSocket socket = new DatagramSocket(8455);
+
+//        准备数据 控制台读取 System.in
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true){
+            String data = reader.readLine();
+
+            byte[] dates = data.getBytes();
+            DatagramPacket packet = new DatagramPacket(dates,0,dates.length,new InetSocketAddress("localhost",8557));
+
+            socket.send(packet);
+            if (reader.equals("bay")){
+                break;
+            }
+
+        }
+
+        socket.close();
+
+
+    }
+}
